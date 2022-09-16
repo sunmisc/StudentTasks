522cb48775a4346f21091de18bf62b26c2d1d278
Sunmisc <zelvalea@gmail.com>
2022-09-16 17:42:31 +0300

    added laboratory3

--- src/main/java/zelvalea/tasks/lab3/MonthsFormatter.java
+++ src/main/java/zelvalea/tasks/lab3/MonthsFormatter.java
@@ -1,26 +1,28 @@
-package zelvalea.tasks;
+package zelvalea.tasks.lab3;
 
-import java.util.Locale;
-import java.util.Map;
 import java.util.Scanner;
 
 public class MonthsFormatter {
 
     public static void main(String[] args) {
+
         try (Scanner scanner = new Scanner(System.in)) {
-            String m = scanner.nextLine().toLowerCase(Locale.ROOT);
 
-            System.out.println(switch (m) {
+            String input = scanner
+                    .nextLine()
+                    .toLowerCase();
+
+            System.out.println(switch (input) {
                 case "декабрь","январь","февраль" -> "зима";
-
                 case "март","апрель","май" -> "весна";
-
                 case "июнь","июль","август" -> "лето";
-
                 case "сентябрь","октябрь","ноябрь" -> "осень";
 
-                default -> "Unknown";
+                default -> "unknown";
             });
+
         }
+
     }
+
 }
---
+++ src/main/java/zelvalea/tasks/lab3/NaNAndInfinity.java
@@ -0,0 +1,32 @@
+package zelvalea.tasks.lab3;
+
+import java.util.Scanner;
+
+public class NaNAndInfinity {
+
+
+    public static void main(String[] args) {
+
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            System.out.printf("""
+                    неопределенность: %s,
+                    положительная бесконечность: %s
+                    отрицательная бесконечность: %s
+                    """,
+                    Double.NaN,
+                    Double.POSITIVE_INFINITY,
+                    Double.NEGATIVE_INFINITY);
+
+
+            while (scanner.hasNext()) {
+                double x = scanner.nextDouble();
+
+                System.out.println(Double.isNaN(Math.sqrt(x))
+                        ? "не число"
+                        : "число"
+                );
+            }
+        }
+    }
+}

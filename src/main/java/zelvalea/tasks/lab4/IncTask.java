f574d5b53d053e53810abc708947ff0b5713a6ef
Sunmisc <zelvalea@gmail.com>
2022-09-22 19:12:34 +0300

    added lab4

--- src/main/java/zelvalea/tasks/lab4/IncTask.java
+++ src/main/java/zelvalea/tasks/lab4/IncTask.java
@@ -5,6 +5,22 @@ import java.util.Scanner;
 public class IncTask {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        if (n < 1)
+            throw new IllegalArgumentException("число должно быть больше нуля");
+
+        int i = 0;
+        int sum = 0;
+        while (i <= n) {
+            sum += i;
+            i++;
+            System.out.println("Индекс: " + i + " Сумма: " + sum);
+        }
+    }
+
+    public static void main0(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             int n = scanner.nextInt();
 

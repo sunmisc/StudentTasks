af0d4ef2077e910e3590177b1cef9205dd86e74d
Sunmisc <zelvalea@gmail.com>
2022-09-22 23:40:09 +0300

    cleanup

--- src/main/java/zelvalea/tasks/lab3/NaNAndInfinity.java
+++ src/main/java/zelvalea/tasks/lab3/NaNAndInfinity.java
@@ -5,7 +5,24 @@ import java.util.Scanner;
 public class NaNAndInfinity {
 
 
+
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("неопределенность: "+00.0/0.0);
+        System.out.println("положительная бесконечность: "+(1.0/0.0));
+        System.out.println("отрицательная бесконечность: "+(-1.0/0.0));
+
+        double x = scanner.nextDouble();
+
+        System.out.println(Double.isNaN(Math.sqrt(x))
+                ? "не число"
+                : "число"
+        );
+    }
+
+    public static void main0(String[] args) {
 
         try (Scanner scanner = new Scanner(System.in)) {
 

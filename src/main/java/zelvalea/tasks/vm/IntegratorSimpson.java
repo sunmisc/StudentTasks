6bf1a29ea285dc99a09239e2de151bc08a4364a2
Sunmisc <zelvalea@gmail.com>
2022-10-19 19:13:51 +0300

    cleanup

--- src/main/java/zelvalea/tasks/vm/IntegratorSimpson.java
+++ src/main/java/zelvalea/tasks/vm/IntegratorSimpson.java
@@ -1,29 +1,24 @@
 package zelvalea.tasks.vm;
 
-import org.apache.commons.math3.analysis.integration.SimpsonIntegrator;
-
-import java.util.function.DoubleUnaryOperator;
 
 public class IntegratorSimpson {
-    static final double ACCURACY = Math.pow(10, -8) * 0.5;
 
     public static void main(String[] args) {
-        System.out.println(simpsonRule(
-                x -> Math.exp(x) * Math.cos(x),
-                0,1, ACCURACY)
-        );
+        System.out.println(simpsonRule(0,1, Math.pow(10, -8) * 0.5));
     }
-    public static double simpsonRule(DoubleUnaryOperator func,
-                                       double a, double b, double accuracy) {
+    private static double func(double x) {
+        return Math.exp(x) * Math.cos(x);
+    }
+    public static double simpsonRule(double a, double b, double accuracy) {
         double dx = b - a;
         double olds = 0;
-        double oldt = 0.5 * (func.applyAsDouble(a) + func.applyAsDouble(b)) * dx;
+        double oldt = 0.5 * (func(a) + func(b)) * dx;
         for (int i = 1; ; i *= 2) {
             double t = 0;
             double spacing = dx / i;
             double x = a + 0.5 * spacing;
             for (int q = 0; q < i; q++) {
-                t += func.applyAsDouble(x);
+                t += func(x);
                 x += spacing;
             }
 

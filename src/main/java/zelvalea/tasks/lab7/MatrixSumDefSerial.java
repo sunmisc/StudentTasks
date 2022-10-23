69e0f545c8e8b3060f6a6f875d75931edb9c453d
Sunmisc <zelvalea@gmail.com>
2022-10-23 18:09:41 +0300

    cleanup

--- src/main/java/zelvalea/tasks/lab7/MatrixSumDefSerial.java
+++ src/main/java/zelvalea/tasks/lab7/MatrixSumDefSerial.java
@@ -1,22 +1,44 @@
 package zelvalea.tasks.lab7;
 
 import java.io.*;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
-import java.util.StringJoiner;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class MatrixSumDefSerial {
+
     public static void main(String[] args) throws IOException {
+        main0();
+    }
+    public static void main0() throws IOException {
+        String resource = "E:\\IdeaProjects\\StudentTasks\\src\\main\\resources\\";
+        String[] args = {
+                resource + "/file1/",
+                resource + "/file2/",
+                resource + "/file3/",
+        };
+        writeMatrix(args[0], new double[][]{
+                {0,1,3},
+                {1,5,9},
+                {6,2,8}
+        });
+        writeMatrix(args[1], new double[][]{
+                {0,1,3},
+                {1,5,9},
+                {6,2,8}
+        });
+        main1(args);
+    }
+
+
+    public static void main1(String[] args) throws IOException {
         if (args.length < 3) throw new IllegalArgumentException();
         String input1 = args[0], input2 = args[1], output = args[2];
 
         try (
-             ObjectInputStream oi1 = new ObjectInputStream(new FileInputStream(input1));
-             ObjectInputStream oi2 = new ObjectInputStream(new FileInputStream(input2));
-             ObjectOutputStream oo = new ObjectOutputStream(new FileOutputStream(output))
+                ObjectInputStream oi1 = new ObjectInputStream(new FileInputStream(input1));
+                ObjectInputStream oi2 = new ObjectInputStream(new FileInputStream(input2));
         ) {
             double[][] matrix1 = (double[][]) oi1.readObject();
             double[][] matrix2 = (double[][]) oi2.readObject();
@@ -30,8 +52,21 @@ public class MatrixSumDefSerial {
                     dataOutput[x][y] = matrix1[x][y] + matrix2[x][y];
                 }
             }
-            oo.writeObject(dataOutput);
+            System.out.println(Arrays.deepToString(dataOutput));
+            writeMatrix(output, dataOutput);
         } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void writeMatrix(String path, double[][] data) throws IOException {
+        Path outputPath = Path.of(path);
+        System.out.println(outputPath.toAbsolutePath());
+        if (Files.notExists(outputPath)) { Files.createFile(outputPath); }
+
+        try (ObjectOutputStream oo = new ObjectOutputStream(Files.newOutputStream(outputPath))) {
+            oo.writeObject(data);
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }

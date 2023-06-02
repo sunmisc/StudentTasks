ab4ca1e1a4f4f283a701332fdc171daeb77f9c58
ZelvaLea <zelvalea@gmail.com>
2023-06-02 19:33:33 +0300

    cleanup

--- src/main/java/zelvalea/tasks/aac_binary/JmhTask4.java
+++ src/main/java/zelvalea/tasks/aac_binary/JmhTask4.java
@@ -1,42 +1,45 @@
 package zelvalea.tasks.aac_binary;
 
 
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+import java.util.concurrent.TimeUnit;
+
 @State(Scope.Thread)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@BenchmarkMode(Mode.Throughput)
+@Warmup(iterations = 2, time = 1)
+@Measurement(iterations = 5, time = 1)
+@Fork(1)
 public class JmhTask4 {
 
+    static final int[] a = new int[]{1,5,2,4,6};
+    static final int[] k = new int[]{2,3,2,3,3};
+
+    @Param({"122", "10000"})
+    int x;
+
+    @Benchmark
+    public int myFind() {
+        return Task4.myComputeDaysToCutTrees(a, k, x);
+    }
+    @Benchmark
+    public int binaryFind() {
+        return Task4.binaryDaysToCutTrees(a, k, x);
+    }
+    @Benchmark
+    public int slowFind() {
+        return Task4.legacyDaysToCutTrees(a, k, x);
+    }
 
     public static void main(String[] args) throws RunnerException {
         Options opt = new OptionsBuilder()
                 .include(JmhTask4.class.getSimpleName())
-                .forks(1)
-                .measurementBatchSize(3)
-                .warmupIterations(1)
                 .build();
         new Runner(opt).run();
-    }
-
-    static final int[] a = new int[]{1, 2, 2, 2};
-    static final int[] k = new int[]{2,2,2,2,2};
-
-    static final int x = 81179;
-
-
-    @Benchmark
-    public int fastFind() {
-
-        return Task4.compute(a, k, x);
-    }
-    @Benchmark
-    public int slowFind() {
-
-        return Task4.legacyCompute(a, k,1,0, x);
     }
 }
--- src/main/java/zelvalea/tasks/aac_binary/Task4.java
+++ src/main/java/zelvalea/tasks/aac_binary/Task4.java
@@ -1,52 +1,71 @@
 package zelvalea.tasks.aac_binary;
 
+
 public class Task4 {
 
     public static void main(String[] args) {
-        /*JmhTask4.fastFind  thrpt    5  1266523,862 ± 35405,870  ops/s
-        JmhTask4.slowFind  thrpt    5      838,789 ±   727,469  ops/s*/
         int[] a = new int[]{1,8,8,8,9};
-        int[] k = new int[]{2,2,2,2,2};
+        int[] k = new int[]{2,2,1,9,1};
 
-        // 0 2 4 8
+        int x = 55;
+        System.out.println(binaryDaysToCutTrees(a,k, x));
+        System.out.println(legacyDaysToCutTrees(a,k, x));
+        System.out.println(myComputeDaysToCutTrees(a,k, x));
 
-        int x = 892322;
-        //System.out.println(legacyCompute(a,k,1, 0, x));
-        System.out.println(compute(a,k, x));
     }
-    public static int legacyCompute(int[] a, int[] k,
-                                    int startDay, int startTrees,
-                                    int target) {
-        for (int n = a.length;; ++startDay) {
-            for (int i = 0; i < n; ++i) {
-                if (startDay % k[i] == 0)
-                    continue;
-                startTrees += a[i];
 
-                if (startTrees >= target)
-                    return startDay;
-            }
+    public static int
+    binaryDaysToCutTrees(int[] a, int[] k, long target) {
+        int left = 1, right = Integer.MAX_VALUE; // min / max days
+        int n = a.length;
+        while (left < right) {
+            int mid = left + ((right - left) >>> 1);
+
+            long treesCut = 0;
+            for (int i = 0; i < n; i++)
+                treesCut += (long) a[i] * (mid / k[i]);
+
+            if (treesCut >= target)
+                right = mid;
+            else
+                left = mid + 1;
+        }
+        return left;
+    }
+
+    public static int
+    legacyDaysToCutTrees(int[] a, int[] k, int target) {
+        for (int n = a.length, days = 1;;) {
+            long treesCut = 0;
+            for (int i = 0; i < n; i++)
+                treesCut += (long) a[i] * (days / k[i]);
+
+            if (treesCut >= target)
+                return days;
+            days++;
         }
     }
-    public static int compute(int[] a, int[] k, long x) {
+    private static int cache;
+    public static int
+    myComputeDaysToCutTrees(int[] a, int[] k, int target) {
 
-        int sum = 0;
-        for (int j : a)
-            sum += j;
-        return compute0(a, k, sum, x);
+        int c = cache;
+        if (c == 0) {
+            int sum = 0;
+            for (int j : a)
+                sum += j;
+            c = cache = sum;
+        }
+        return compute0(a, k, c, target) + 1;
     }
     private static int
     compute0(int[] a, int[] k, int sum, long x) {
-
-        int estimatedDays = (int)Math.ceilDiv(x, sum),
-                i, n;
+        int estimatedDays = (int) (x / sum);
         long cntTrees = x;
-        for (i = 0, n = a.length; i < n; ++i) {
-            int m = k[i];
-            long h = Math.floorDiv((m-1)*estimatedDays, m);
-            cntTrees -= h * a[i];
-        }
-        System.out.println(cntTrees + " | " + x + " " + estimatedDays);
+
+        for (int i = 0, n = a.length; i < n; ++i)
+            cntTrees -= (long) (estimatedDays / k[i]) * a[i];
+
         return cntTrees >= x ? estimatedDays
                 : estimatedDays + compute0(a, k, sum, cntTrees);
     }
